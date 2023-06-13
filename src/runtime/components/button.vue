9ef7bb3a70b442542917729876908177e046898c
Elon Salfati <elon@salfati.group>
2023-06-12 21:10:02 -0700

    fix: updated button

--- src/runtime/components/button.vue
+++ src/runtime/components/button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="px-2 py-1.5 w-full rounded bg-brand-500 text-white font-medium hover:bg-brand-600 transition-all ease-in-out focus:outline-none focus:ring-1 focus:ring-brand-500/80 focus:ring-offset-1 focus:ring-offset-brand-500/20 focus:bg-brand-500/20 focus:text-brand-600 disabled:opacity-50 disabled:cursor-not-allowed"
+    class="px-2 py-1.5 w-full rounded bg-brand-500 text-white font-medium hover:bg-brand-600 transition-all ease-in-out focus:outline-none focus:ring-1 focus:ring-brand-500/80 focus:ring-offset-1 focus:ring-offset-brand-500/20 focus:bg-brand-500/20 focus:text-brand-600 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
     :disabled="disabled"
     :class="{
       'cursor-not-allowed bg-brand-gray-500/50': disabled,
