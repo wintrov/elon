2a99551e4ff6999932691bf1c336468a58d1044d
Omer Hochman <omer.hochman@gmail.com>
2023-07-11 14:00:47 +0300

    Add simple dropdown component

--- playground/pages/dropdown/index.vue
+++ playground/pages/dropdown/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Hi</div>
+  <div class="w-full flex flex-col space-y-2 px-[40%] mt-10">
+    <k-dropdown :items="items" title="Menu"></k-dropdown>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const items = [
+  {
+    label: 'Option 1',
+    value: 'option-1',
+    onClick: () => console.log('option 1 clicked'),
+  },
+  { label: 'Option 2', value: 'option-2' },
+]
+</script>
