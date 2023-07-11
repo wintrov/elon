016bb78aa12dc348cac0a021f031d0f3e9528cf5
Omer Hochman <omer.hochman@gmail.com>
2023-07-11 15:06:20 +0300

    Add basic badge component

---
+++ playground/pages/badge/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <a href="/"> back</a>
+  <div class="w-full flex flex-col space-y-2 px-[40%] mt-10 [&>*]:w-fit">
+    <k-badge variant="primary">Primary</k-badge>
+    <k-badge variant="primary" dot>Primary Dot</k-badge>
+    <k-badge variant="primary" button>Primary Button</k-badge>
+    <k-badge variant="primary" flat>Primary Flat</k-badge>
+    <k-badge variant="primary" button flat>Primary Button Flat</k-badge>
+    <k-badge variant="primary" dot flat>Primary Dot Flat</k-badge>
+
+    <k-badge variant="secondary">Seondary</k-badge>
+    <k-badge variant="info">Info</k-badge>
+    <k-badge variant="success">Success</k-badge>
+    <k-badge variant="warn">Warn</k-badge>
+    <k-badge variant="danger">Danger</k-badge>
+
+    <k-badge size="sm">Small</k-badge>
+    <k-badge size="md">Medium</k-badge>
+
+    <k-badge pill>Pill</k-badge>
+  </div>
+</template>
