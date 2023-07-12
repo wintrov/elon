b2666db4fde191f4f4ca0d5b936e3ef061655acb
Omer Hochman <omer.hochman@gmail.com>
2023-07-12 07:16:07 +0300

    Add Select - DRAFT

--- playground/pages/select/index.vue
+++ playground/pages/select/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="w-full flex flex-col space-y-2 px-[40%] mt-10">
-    <k-select :options="options" />
+  <div class="w-full flex flex-col space-y-2 mt-10">
+    <k-select :options="people" :modelValue="modelValue" />
   </div>
 </template>
 
 <script setup lang="ts">
-const options = [
-  {
-    label: 'Option 1',
-    value: 'option-1',
-  },
-  {
-    label: 'Option 2',
-    value: 'option-2',
-  },
-  {
-    label: 'Option 3',
-    value: 'option-3',
-  },
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+  { id: 7, name: 'Caroline Schultz' },
+  { id: 8, name: 'Mason Heaney' },
+  { id: 9, name: 'Claudie Smitham' },
+  { id: 10, name: 'Emil Schaefer' },
 ]
+
+const modelValue = [people[1], people[2]]
 </script>
